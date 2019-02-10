eb8f5a3de90ac2e9634d33e73abff75d6f28f1e5
CristianRichie <cristianassaiante@outlook.com>
2019-02-10 04:00:35 +0100

    improved testing environment

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,7 +2,11 @@
 
 echo ""
 for i in $(find . -exec file {} \; | grep -i elf | awk '{print $1}' | awk '{print substr($0, 1, length($0)-1)}'); do
+    echo "***** RUNNING vagrind $i *****";
+    valgrind $i
+    echo ""
     echo "***** RUNNING $i *****";
     $i
+    echo ""
     echo ""
 done;
