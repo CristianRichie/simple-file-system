d4d0520624197bd8058484a017f5dd394b46f8bf
CristianRichie <cristianassaiante@outlook.com>
2019-02-25 20:40:20 +0100

    simplefs seek completed

--- src/simplefs.c
+++ src/simplefs.c
@@ -7,22 +7,20 @@ DirectoryHandle* SimpleFS_init(SimpleFS* fs, DiskDriver* disk) {
 
     fs->disk = disk;
 
-    FirstDirectoryBlock * first_dir_block = malloc(BLOCK_SIZE);
-    if (!first_dir_block)
+    FirstDirectoryBlock first_dir_block;
+    int ret = DiskDriver_readBlock(disk, &first_dir_block, 0);
+    if (ret == -1) 
         return NULL;
-    int ret = DiskDriver_readBlock(disk, first_dir_block, 0);
-    if (ret == -1) {
-        free(first_dir_block);
-        return NULL;
-    }
+
+    fs->current_directory_block = &first_dir_block;
 
     DirectoryHandle * dir_handle = malloc(sizeof(DirectoryHandle));
     if (!dir_handle) 
         return NULL;
     dir_handle->sfs = fs;
-    dir_handle->dcb = first_dir_block;
+    dir_handle->dcb = &first_dir_block;
     dir_handle->directory = NULL;
-    dir_handle->current_block = &first_dir_block->header;
+    dir_handle->current_block = &first_dir_block.header;
     dir_handle->pos_in_dir = 0;
     dir_handle->pos_in_block = 0;
 
@@ -79,7 +77,48 @@ int SimpleFS_read(FileHandle* f, void* data, int size) {
 }
 
 int SimpleFS_seek(FileHandle* f, int pos) {
-    return -1;
+    if (!f) 
+        return -1;
+
+    int ret;
+
+    int data_size = BLOCK_SIZE - sizeof(BlockHeader);
+    int target_block = pos % data_size;
+    if (target_block == f->current_block->block_in_file) {
+        f->pos_in_file = pos;
+        return pos;
+    }
+    else if (target_block < f->current_block->block_in_file) {
+        BlockHeader future_header;
+        do {
+            int prev_block = f->current_block->previous_block;
+            FirstFileBlock block;
+            ret = DiskDriver_readBlock(f->sfs->disk, &block, prev_block);
+            if (ret == -1)
+                return -1;
+            future_header = block.header;
+            
+        } while(future_header.block_in_file != target);
+        f->pos_in_file = pos;
+        f->current_block = &future_header;
+        return pos;
+    }
+    else {
+        BlockHeader future_header;
+        do {
+            int next_block = f->current_block->next_block;
+            if (next_block == -1)
+                return -1;
+            FirstFileBlock block;
+            ret = DiskDriver_readBlock(f->sfs->disk, &block, next_block);
+            if (ret == -1)
+                return -1;
+            future_header = block.header;
+        } while(future_header.block_in_file != target);
+        f->pos_in_file = pos;
+        f->current_block = &future_header;
+        return pos;
+    }
 }
 
 int SimpleFS_changeDir(SimpleFS* fs, char* dirname) {
@@ -87,7 +126,8 @@ int SimpleFS_changeDir(SimpleFS* fs, char* dirname) {
 }
 
 int SimpleFS_mkDir(SimpleFS* fs, char* dirname) {
-    if (!fs) return -1;
+    if (!fs) 
+        return -1;
 
     int free_block = DiskDriver_getFreeBlock(fs->disk, fs->current_directory_block.fcb.block_in_disk);
     if (free_block == -1)
@@ -105,9 +145,7 @@ int SimpleFS_mkDir(SimpleFS* fs, char* dirname) {
     fdb.fcb.is_dir = 1;
     fdb.num_entries = 0;
 
-    int ret = DiskDriver_writeBlock(fs->disk, &fdb, free_block);
-    if (ret == -1) 
-        return -1;
+    return DiskDriver_writeBlock(fs->disk, &fdb, free_block);
 }
 
 int SimpleFS_remove(SimpleFS* fs, char* filename) {
