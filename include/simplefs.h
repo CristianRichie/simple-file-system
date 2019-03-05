7fa133ee5aaf6c258bb5199bcbff04fc8a7f4338
CristianRichie <cristianassaiante@outlook.com>
2019-03-05 17:42:58 +0100

    restart sfs, wrong idea

--- include/simplefs.h
+++ include/simplefs.h
@@ -32,7 +32,7 @@ typedef struct {
 typedef struct {
   BlockHeader header;
   FileControlBlock fcb;
-  char data[BLOCK_SIZE-sizeof(FileControlBlock) - sizeof(BlockHeader)];
+  char data[BLOCK_SIZE-sizeof(FileControlBlock) - sizeof(BlockHeader)] ;
 } FirstFileBlock;
 
 // this is one of the next physical blocks of a file
@@ -66,7 +66,6 @@ typedef struct {
 typedef struct {
   DiskDriver* disk;
   // add more fields if needed
-  FirstDirectoryBlock* current_directory_block;
 } SimpleFS;
 
 // this is a file handle, used to refer to open files
@@ -78,7 +77,6 @@ typedef struct {
   int pos_in_file;                 // position of the cursor
 } FileHandle;
 
-/*
 typedef struct {
   SimpleFS* sfs;                   // pointer to memory file system structure
   FirstDirectoryBlock* dcb;        // pointer to the first block of the directory(read it)
@@ -87,10 +85,10 @@ typedef struct {
   int pos_in_dir;                  // absolute position of the cursor in the directory
   int pos_in_block;                // relative position of the cursor in the block
 } DirectoryHandle;
-*/
 
 // initializes a file system on an already made disk
-void SimpleFS_init(SimpleFS* fs, DiskDriver* disk);
+// returns a handle to the top level directory stored in the first block
+DirectoryHandle* SimpleFS_init(SimpleFS* fs, DiskDriver* disk);
 
 // creates the inital structures, the top level directory
 // has name "/" and its control block is in the first position
@@ -99,17 +97,18 @@ void SimpleFS_init(SimpleFS* fs, DiskDriver* disk);
 // and set to the top level directory
 void SimpleFS_format(SimpleFS* fs);
 
-// creates an empty file in the current directory
+// creates an empty file in the directory d
 // returns null on error (file existing, no free blocks)
 // an empty file consists only of a block of type FirstBlock
-FileHandle* SimpleFS_createFile(SimpleFS* fs, const char* filename);
+FileHandle* SimpleFS_createFile(DirectoryHandle* d, const char* filename);
 
 // reads in the (preallocated) blocks array, the name of all files in a directory 
-// the directory must be inside the current one
-int SimpleFS_readDir(char** names, char* dirname);
+int SimpleFS_readDir(char** names, DirectoryHandle* d);
 
-// opens a file in the current directory. The file should be exisiting
-FileHandle* SimpleFS_openFile(SimpleFS* fs, const char* filename);
+
+// opens a file in the  directory d. The file should be exisiting
+FileHandle* SimpleFS_openFile(DirectoryHandle* d, const char* filename);
+
 
 // closes a file handle (destroyes it)
 int SimpleFS_close(FileHandle* f);
@@ -119,8 +118,9 @@ int SimpleFS_close(FileHandle* f);
 // returns the number of bytes written
 int SimpleFS_write(FileHandle* f, void* data, int size);
 
-// returns the number of bytes read (moving the current pointer to pos)
-// -1 on error
+// writes in the file, at current position size bytes stored in data
+// overwriting and allocating new space if necessary
+// returns the number of bytes read
 int SimpleFS_read(FileHandle* f, void* data, int size);
 
 // moves the current pointer to pos
@@ -128,16 +128,15 @@ int SimpleFS_read(FileHandle* f, void* data, int size);
 // -1 on error (file too short)
 int SimpleFS_seek(FileHandle* f, int pos);
 
-// seeks for a directory in the current directory. 
-// if dirname is equal to ".." it goes one level up
+// seeks for a directory in d. If dirname is equal to ".." it goes one level up
 // 0 on success, negative value on error
 // it does side effect on the provided handle
- int SimpleFS_changeDir(SimpleFS* fs, char* dirname);
+ int SimpleFS_changeDir(DirectoryHandle* d, char* dirname);
 
 // creates a new directory in the current one (stored in fs->current_directory_block)
 // 0 on success
 // -1 on error
-int SimpleFS_mkDir(SimpleFS* fs, char* dirname);
+int SimpleFS_mkDir(DirectoryHandle* d, char* dirname);
 
 // removes the file in the current directory
 // returns -1 on failure 0 on success
@@ -146,4 +145,6 @@ int SimpleFS_remove(SimpleFS* fs, char* filename);
 
 
   
+
+
 
--- src/simplefs.c
+++ src/simplefs.c
@@ -3,58 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-voi SimpleFS_init(SimpleFS* fs, DiskDriver* disk) {
-
-    fs->disk = disk;
-
-    FirstDirectoryBlock first_dir_block;
-    int ret = DiskDriver_readBlock(disk, &first_dir_block, 0);
-    if (ret == -1) 
-        return NULL;
-
-    fs->current_directory_block = &first_dir_block;
-
-    /*
-    DirectoryHandle * dir_handle = malloc(sizeof(DirectoryHandle));
-    if (!dir_handle) 
-        return NULL;
-    dir_handle->sfs = fs;
-    dir_handle->dcb = &first_dir_block;
-    dir_handle->directory = NULL;
-    dir_handle->current_block = &first_dir_block.header;
-    dir_handle->pos_in_dir = 0;
-    dir_handle->pos_in_block = 0;
-
-    return dir_handle;
-    */
+DirectoryHandle* SimpleFS_init(SimpleFS* fs, DiskDriver* disk) {
+    return NULL;
 }
 
 void SimpleFS_format(SimpleFS* fs) {
-    int bmap_size = fs->disk->header->bitmap_entries;
-    bzero(fs->disk->bitmap_data, bmap_size);
-    fs->disk->header->free_blocks = fs->disk->header->num_blocks;
-    fs->disk->header->first_free_block = 0;
-
-    FirstDirectoryBlock fdb;
-    fdb.header.previous_block = -1;
-    fdb.header.next_block = -1;
-    fdb.header.block_in_file = 0;
-    fdb.fcb.directory_block = -1;
-    fdb.fcb.block_in_disk = 0;
-    strcpy(fdb.fcb.name, "/");
-    fdb.fcb.size_in_bytes = BLOCK_SIZE;
-    fdb.fcb.size_in_blocks = 1;
-    fdb.fcb.is_dir = 1;
-    fdb.num_entries = 0;
-
-    int ret = DiskDriver_writeBlock(fs->disk, &fdb, 0);
-    if (ret == -1) 
-        return;
-
-    fs->current_directory_block = &fdb;
+    return;
 }
 
-FileHandle* SimpleFS_createFile(SimpleFS* fs, const char* filename) {
+FileHandle* SimpleFS_createFile(DirectoryHandle* d, const char* filename) {
     return NULL;
 }
 
@@ -62,15 +19,12 @@ int SimpleFS_readDir(char** names, DirectoryHandle* d) {
     return -1;
 }
 
-FileHandle* SimpleFS_openFile(SimpleFS* fs, const char* filename) {
+FileHandle* SimpleFS_openFile(DirectoryHandle* d, const char* filename) {
     return NULL;
 }
 
 int SimpleFS_close(FileHandle* f) {
-    if (!f) 
-        return -1;
-    free(f);
-    return 0;
+    return -1;
 }
 
 int SimpleFS_write(FileHandle* f, void* data, int size) {
@@ -82,75 +36,15 @@ int SimpleFS_read(FileHandle* f, void* data, int size) {
 }
 
 int SimpleFS_seek(FileHandle* f, int pos) {
-    if (!f) 
-        return -1;
-
-    int ret;
-
-    int data_size = BLOCK_SIZE - sizeof(BlockHeader);
-    int target_block = pos % data_size;
-    if (target_block == f->current_block->block_in_file) {
-        f->pos_in_file = pos;
-        return pos;
-    }
-    else if (target_block < f->current_block->block_in_file) {
-        BlockHeader future_header;
-        do {
-            int prev_block = f->current_block->previous_block;
-            FirstFileBlock block;
-            ret = DiskDriver_readBlock(f->sfs->disk, &block, prev_block);
-            if (ret == -1)
-                return -1;
-            future_header = block.header;
-            
-        } while(future_header.block_in_file != target_block);
-        f->pos_in_file = pos;
-        f->current_block = &future_header;
-        return pos;
-    }
-    else {
-        BlockHeader future_header;
-        do {
-            int next_block = f->current_block->next_block;
-            if (next_block == -1)
-                return -1;
-            FirstFileBlock block;
-            ret = DiskDriver_readBlock(f->sfs->disk, &block, next_block);
-            if (ret == -1)
-                return -1;
-            future_header = block.header;
-        } while(future_header.block_in_file != target_block);
-        f->pos_in_file = pos;
-        f->current_block = &future_header;
-        return pos;
-    }
-}
-
-int SimpleFS_changeDir(SimpleFS* fs, char* dirname) {
     return -1;
 }
 
-int SimpleFS_mkDir(SimpleFS* fs, char* dirname) {
-    if (!fs) 
-        return -1;
+int SimpleFS_changeDir(DirectoryHandle* d, char* dirname) {
+    return -1;
+}
 
-    int free_block = DiskDriver_getFreeBlock(fs->disk, fs->current_directory_block->fcb.block_in_disk);
-    if (free_block == -1)
-        return -1;
-
-    FirstDirectoryBlock fdb;
-    fdb.header.previous_block = -1;
-    fdb.header.next_block = -1;
-    fdb.header.block_in_file = 0;
-    fdb.fcb.directory_block = fs->current_directory_block->fcb.block_in_disk;
-    fdb.fcb.block_in_disk = free_block;
-    strcpy(fdb.fcb.name, dirname);
-    fdb.fcb.size_in_bytes = BLOCK_SIZE;
-    fdb.fcb.size_in_blocks = 1;
-    fdb.fcb.is_dir = 1;
-    fdb.num_entries = 0;
-
-    return DiskDriver_writeBlock(fs->disk, &fdb, free_block);
+int SimpleFS_mkDir(DirectoryHandle* d, char* dirname) {
+    return -1;
 }
 
 int SimpleFS_remove(SimpleFS* fs, char* filename) {
